98cf2677be27e40f6c6ede7f06bd8a18e1782c58
zak <zak.j.holt@gmail.com>
2016-08-30 14:36:19 -0400

    About to start working on V2

--- app.js
+++ app.js
@@ -9,37 +9,35 @@ var Person = {
 
 var contactList = [];
 
-var current;
-
 $(document).ready(function() {
 
     $("#addContact").click(function(e) {
         e.preventDefault();
         if ($("#firstName").val() !== "" && $("#lastName").val() !== "") {
-          var newItem = Object.create(Person);
-          for (var prop in newItem) {
-              newItem[prop] = $("#" + prop).val();
-          }
-          contactList.push(newItem);
-          $("#contactList").append("<li>" + newItem.firstName + " " + newItem.lastName + "</li>");
-          $("input").val("");
+            var newItem = Object.create(Person);
+            for (var prop in newItem) {
+                newItem[prop] = $("#" + prop).val();
+            }
+            contactList.push(newItem);
+            $("#contactList").append("<li>" + newItem.firstName + " " + newItem.lastName + "</li>");
+            $("input").val("");
         } else {
-          alert("First and Last names are required");
+            alert("First and Last names are required");
         }
 
     });
 
     $("#contactList").on("click", "li", function() {
-      $("form").css("width", "66.66%");
-      $("#contactInfo").css("display", "inline");
-      //Populate the info card with information from the corresponding object.
-      var item = contactList[$(this).index()];
-      $("#infoTitle").text(item.firstName + " " + item.lastName);
-      $("#infoFirstName").text(item.firstName);
-      $("#infoLastName").text(item.lastName);
-      $("#infoPhoneNumber").text(item.phoneNumber);
-      $("#infoAddresses").empty();
-      $("#infoAddresses").append("<li>" + item.street + ' ' + item.city + ', ' + item.state + "</li>");
+        $("form").css("width", "66.66%");
+        $("#contactInfo").css("display", "inline");
+        //Populate the info card with information from the corresponding object.
+        var item = contactList[$(this).index()];
+        $("#infoTitle").text(item.firstName + " " + item.lastName);
+        $("#infoFirstName").text(item.firstName);
+        $("#infoLastName").text(item.lastName);
+        $("#infoPhoneNumber").text(item.phoneNumber);
+        $("#infoAddresses").empty();
+        $("#infoAddresses").append("<li>" + item.street + ' ' + item.city + ', ' + item.state + "</li>");
 
     });
 });
