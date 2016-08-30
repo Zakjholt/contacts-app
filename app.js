586d22900e5f55631ed29393b4f2604f6f07fd64
zak <zak.j.holt@gmail.com>
2016-08-29 21:27:35 -0400

    can't figure out how to get contact info to display

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 var Person = {
-  firstName: null,
-  lastName: null,
-  phoneNumber: null,
-  street: null,
-  city: null,
-  state: null
+    firstName: null,
+    lastName: null,
+    phoneNumber: null,
+    street: null,
+    city: null,
+    state: null
 };
 
-var contactList= [];
+var contactList = [];
+
+var current;
 
 $(document).ready(function() {
 
-  $("button").click(function() {
-    event.preventDefault();
-    var newItem = Object.create(Person);
-    for (var prop in newItem) {
-      newItem[prop] = $("#" + prop).val();
-    }
-    contactList.push(newItem);
-    $("#contactList").append("<li><a>" + newItem.firstName + " " + newItem.lastName + "</a></li>");
-  });
+    $("button").click(function(e) {
+        e.preventDefault();
+        var newItem = Object.create(Person);
+        for (var prop in newItem) {
+            newItem[prop] = $("#" + prop).val();
+        }
+        contactList.push(newItem);
+        $("#contactList").append("<li id='" + newItem.firstName + "'>" + newItem.firstName + " " + newItem.lastName + "</li>");
+    });
 
+    $("#contactList a").click(function() {
+      $("form").css("width", "66.6666%");
+      $("aside").show();
+    });
 });
